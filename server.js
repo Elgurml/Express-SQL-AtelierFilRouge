5114d9d82f0df4d041d9d1b448026bdde2caa89f
Elgur <leroy.maxence@yahoo.com>
2019-12-04 18:22:35 +0100

    done

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const connection = require('./conf')
-
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 
 app.get('/', (req, res)=>{
     res.status(200).send('server is live!')
 })
+
 
 app.get('/allcards', (req,res)=>{
     connection.query('SELECT * FROM mtgcards', (err, result)=>{
@@ -16,6 +22,7 @@ app.get('/allcards', (req,res)=>{
         }
     })
 })
+
 
 app.get('/cardslegality', (req,res)=>{
     connection.query('SELECT cardname, modernlegal FROM mtgcards', (err, result)=>{
@@ -57,11 +64,10 @@ app.get('/filter3', (req,res)=>{
     })
 })
 
-////// :order n'est pas pris en compte /////
-
 app.get('/allcards/:order', (req,res)=>{
     const order = req.params.order
-    connection.query('SELECT * FROM mtgcards ORDER BY cardname ? ', order, (err, result)=>{
+    console.log(order);
+    connection.query(`SELECT * FROM mtgcards ORDER BY cardname ${order}`, (err, result)=>{
         if(err){
             res.status(500).send('Internal server error')
         }else{
@@ -70,7 +76,66 @@ app.get('/allcards/:order', (req,res)=>{
     })
 })
 
+app.post('/allcards', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO mtgcards SET ?', formData, (err, results) => {
+        if (err) {
+        console.log(err);
+        res.status(500).send("Error saving an employee");
+        } else {
+        res.sendStatus(200);
+        }
+    });
+});
 
+app.put('/mtgcards/:id', (req, res) => {
+    const idCard = req.params.id;
+    const formData = req.body;
+    connection.query('UPDATE mtgcards SET ? WHERE id = ?', [formData, idCard], err => {
+        if (err) {
+        console.log(err);
+        res.status(500).send("Error editing a card");
+        } else {
+        res.sendStatus(200);
+        }
+    });
+});
+
+app.put('/mtgcards/:id', (req, res) => {
+    const idCard = req.params.id;
+    connection.query('UPDATE mtgcards SET modernlegal=!modernlegal WHERE id = ?', idCard, err => {
+        if (err) {
+        console.log(err);
+        res.status(500).send("Error editing a card");
+        } else {
+        res.sendStatus(200);
+        }
+    });
+});
+
+app.delete('/mtgcards/:id', (req, res) => {
+    const idCard = req.params.id;
+    connection.query('DELETE FROM mtgcards WHERE id = ?', [idCard], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error deleting a card");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+app.delete('/mtgcards/', (req, res) => {
+    const idCard = req.params.id;
+    connection.query('DELETE FROM mtgcards WHERE modernlegal = 0 ', [idCard], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error deleting a card");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
 
 app.listen(4000, err=>{
     if(err){
